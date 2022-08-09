5afeba9049134533b4a2e3cbbf5e62560d2115c3
Falashade Greene <falashadegreene@gmail.com>
2022-08-09 17:27:48 -0400

    corrected path within code

--- api/capital.py
+++ api/capital.py
@@ -1,34 +1,34 @@
+
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
+
+# create a function that handles a query
+# server should handle a Get request with a given capital name that responds with a string
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
         url_path = parse.urlsplit(self.path)
         query_string_list = parse.parse_qsl(url_path.query)
-        query_dic = dict(query_string_list)
+        dictionary = dict(query_string_list)
 
-        if "name" in query_dic:
+        if "name" in dictionary:
             url = "https://restcountries.com/v3.1/capital/"
-            query = query_dic["name"]
+            query = dictionary["name"]
 
-            response = requests.get(url + query_dic["name"])
+            response = requests.get(url + dictionary["name"])
 
             data = response.json()
 
-            country = data[0]["name"]
-            country_name = str(country["common"])
-            message = f"{query} is the capital of {country_name}"
-        else:
-            message = "give a name"
+            country_name = data[0]["name"]
+            answer = str(country_name["common"])
+            results = f"{query} is the capital of {answer}"
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(message.encode())
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(results.encode())
 
         return
-
--- api/country.py
+++ api/country.py
@@ -1,32 +1,32 @@
+
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
+# create a function that handles a query
+# server should handle a Get request with a given country name that responds with a string
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         url_path = parse.urlsplit(self.path)
         query_string_list = parse.parse_qsl(url_path.query)
-        query_dict = dict(query_string_list)
+        query_dictionary = dict(query_string_list)
 
-        if "capital" in query_dict:
+        if "capital" in query_dictionary:
             url = "https://restcountries.com/v3.1/capital/"
-            query = query_dict["capital"]
+            query = query_dictionary["capital"]
 
             response = requests.get(url + query)
             data = response.json()
 
             country_name = data[0]["name"]
-            country_str = str(country_name["common"])
-            message = f"The capital of {country_str} is {query}"
-        # else:
-        #     message = "provide name of country"
+            country = str(country_name["common"])
+            message = f"The capital of {country} is {query}"
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(message.encode())
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(message.encode())
 
         return
-
